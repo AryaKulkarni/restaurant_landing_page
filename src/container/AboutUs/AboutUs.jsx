12bda4928928d441f8c5e1ea3c7818ef0449ca16
Arya Kulkarni <kulkarni.arya1209@gmail.com>
2022-11-16 18:08:52 +0530

    initial-commit

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import './AboutUs.css';
+import {images} from '../../constants';
+
+const AboutUs = () => (
+  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+    <div className='app__aboutus-overlay flex__center'>
+      <img src={images.G} alt=""/>
+    </div>
+    <div className='app__aboutus-content flex__center'>
+      <div className='app__aboutus-content_about'>
+        <h1 className='headtext__cormorant'>About Us</h1>
+        <img src={images.spoon} alt="" className='spoon-img'/>
+        <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button className='custom__button'>Explore More</button>
+      </div>
+
+      <div className='app__aboutus-content_knife'>
+        <img src={images.knife} alt=""/>
+      </div>
+
+      <div className='app__aboutus-content_history'>
+        <h1 className='headtext__cormorant'>Our history</h1>
+        <img src={images.spoon} alt="" className='spoon-img'/>
+        <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button className='custom__button'>Explore More</button>
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutUs;
